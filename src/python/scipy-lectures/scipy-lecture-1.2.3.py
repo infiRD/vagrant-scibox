70a01fa92e585af3f39af3b1fcd39830102421ec
Martin Zamba <martin.zamba@gmail.com>
2017-02-14 14:21:43 +0100

    some more progress on numpy-lectures

--- src/python/scipy-lectures/scipy-lecture-1.2.3.py
+++ src/python/scipy-lectures/scipy-lecture-1.2.3.py
@@ -6,6 +6,8 @@ http://www.scipy-lectures.org/intro/language/control_flow.html
 Created on Mon Feb 14 00:19:00 2017
 @author: Martin Zamba
 """
+import matplotlib.pyplot as mp
+import numpy as np
 
 #%% if/then/else
 
@@ -87,13 +89,49 @@ for c in msg:
         msgVowels.append(c)
 print(msgVowels)  # ['e', 'o', 'o', 'a', 'e', 'y', 'o', 'u']
 
+for w in msg.split():
+    print(w)
+
+for w in "Hello how are you?".split(): print(w)
+
+# iterating with index
+words = msg.split()
+for i in range(0, len(words)): print((i, words[i]))
 
 
+# iterating over list with enumerate function
+print()
+for index, item in enumerate(words):
+    print((index, item))
 
+# iterating over dictionary:
+print()
+d = {'a': 1, 'b':1.2, 'c':1j}
+for key, val in sorted(d.items()):
+    print('Key: %s has value: %s' % (key, val))
 
+# list conprehension:
+list = [i**2 for i in range(4)]
 
+#%% ============================  EXCERCISE  =============================
+# Compute the decimals of Pi using the Wallis formula:
+# ------------------------------------------------------------------------
 
+xs = []
+ys = []
+n = 10000
+acc = 1
+i = 1
+while i <= n:
+    acc = acc * 4*i**2 / (4*i**2-1)
+    i = i + 1
+    xs.append(i)
+    ys.append(acc)
 
+pi = 2 * acc
+print("Pi = " + str(pi))
 
+mp.close()
+mp.semilogx(xs, [y*2 for y in ys])
 
 
