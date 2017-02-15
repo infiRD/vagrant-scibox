8a383df1ffac774ad7bcd4dbf7b21cf92e08658d
Martin Zamba <martin.zamba@gmail.com>
2017-02-15 05:20:37 +0100

    added apt-get update in crucial.sh

--- scripts/crucial.sh
+++ scripts/crucial.sh
@@ -3,6 +3,7 @@
 # ================== install crucial packages =======================
 sudo ./waitForApt.sh
 sudo dpkg --configure -a
+sudo apt-get update --fix-missing
 
 sudo apt-get install -y -q curl unzip git zsh autogen automake \
                            autoconf build-essential checkinstall \
